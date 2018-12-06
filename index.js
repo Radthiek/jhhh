61a756d17d1830c7684bdcce298dbc23f3699d16
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-12-06 19:25:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
   client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`**هلا نورت سيرفر حياك لسيرفرنا : {https://discord.gg/zuFJ7UP}**`) 
+  return channel.send(`**هلا  نورت  سيرفر   تبقى دورك  هنا افضل  شوب واكواد  مجانيه ورهيبة ادخل وبس ** [https://discord.gg/zuFJ7UP]`) 
 }).catch(console.error)
 })
 
